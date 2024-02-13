22ce21303152f7b7342e0dfba359d1f550f974bc
Demonty6 <carlosalfonsolopezramos1993@gmail.com>
2024-02-12 22:22:16 -0500

    getting ready for deployment

--- client/src/app/modules/website/website.component.ts
+++ client/src/app/modules/website/website.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-website',
@@ -6,9 +8,15 @@ import { Component } from '@angular/core';
 })
 export class WebsiteComponent {
 
+  constructor(private router:Router){}
 
-
-  onActivate(event : any){
-    window.scrollTo(0,0) 
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event) => {
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    })
+    
   }
+
 }
